070f72acd1847f5bd539ebe6371467667bb9edde
Rita <marharitavol@gmail.com>
2024-09-06 01:08:41 +0300

    added new button to onboarding

--- FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -20,20 +20,36 @@ class OnboardingCoordinator: Coordinator {
 
 private extension OnboardingCoordinator {
     func showOnboarding() {
-        var pages = [UIViewController]()
+        var pages = [OnboardingPartViewController]()
         
-        let firstVC = UIViewController()
-        firstVC.view.backgroundColor = .purple
+        let firstVC = OnboardingPartViewController()
+        firstVC.imageToShow = UIImage(named: "chicken-leg")
+        firstVC.titleText = "Delicious Food"
+        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        firstVC.buttonText = "Next"
         
-        let secondVC = UIViewController()
-        secondVC.view.backgroundColor = .yellow
+        let secondVC = OnboardingPartViewController()
+        secondVC.imageToShow = UIImage(named: "shipped")
+        secondVC.titleText = "Fast Shipping"
+        secondVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        secondVC.buttonText = "Next"
         
-        let thirdVC = UIViewController()
-        thirdVC.view.backgroundColor = .red
+        let thirdVC = OnboardingPartViewController()
+        thirdVC.imageToShow = UIImage(named: "medal")
+        thirdVC.titleText = "Certificate Food"
+        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        thirdVC.buttonText = "Next"
+        
+        let fourthVC = OnboardingPartViewController()
+        fourthVC.imageToShow = UIImage(named: "credit-card")
+        fourthVC.titleText = "Payment Online"
+        fourthVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        fourthVC.buttonText = "Cool"
         
         pages.append(firstVC)
         pages.append(secondVC)
         pages.append(thirdVC)
+        pages.append(fourthVC)
         
         let presenter = OnboardingViewPresenter(coordinator: self)
         let viewController = OnboardingViewController(pages: pages, viewOutPut: presenter)
